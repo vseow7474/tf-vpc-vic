e203b4a00fdff9c3d76c0cb5434b3ef67e81511e
vseow7474 <vseow7474@gmail.com>
2024-11-28 21:46:43 +0800

    Add output for private subnets id

--- output.tf
+++ output.tf
@@ -1,6 +1,11 @@
 output "nat_gateway_ids" {
   value = module.vpc.natgw_ids
 }
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
 output "aws_availability_zones" {
   value = data.aws_availability_zones.available.names
 }
